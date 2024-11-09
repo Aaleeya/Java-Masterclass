441bb4edbacd56eae91b2125c4c13d0d8f36cd8c
Aaleeya <shaikhaaleeya17@gmail.com>
2024-11-08 19:44:11 -0500

    OOPS concepts of Classes, Objects, getter and setter methods related code added

---
+++ src/OOPSConcepts/Airfryer.java
@@ -0,0 +1,60 @@
+package OOPSConcepts;
+
+public class Airfryer {
+    private String model= "2002";
+    private String make = "Philips";
+    private String color= "black";
+    private double capacity= 4.4;
+    private boolean window = true;
+
+    //Getter and Setter methods when instance fields of class are private
+    public String getModel() {
+        return model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public boolean isWindow() {
+        return window;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setWindow(boolean window) {
+        this.window = window;
+    }
+
+    public void describeAirFryer(){
+        System.out.println(capacity +" - Capacity "+
+                color+" color "+
+                make+ " make "+
+                model+ " model "+
+                (window ? " hasWindow " : ""));
+    }
+
+
+}
---
+++ src/OOPSConcepts/Main.java
@@ -0,0 +1,12 @@
+package OOPSConcepts;
+
+public class Main {
+    public static void main(String[] args) {
+        Airfryer af = new Airfryer();
+        af.describeAirFryer();
+        System.out.println(af.getMake());
+        System.out.println(af.getColor());
+        af.setMake("Ninja");
+        System.out.println(af.getMake());
+    }
+}
