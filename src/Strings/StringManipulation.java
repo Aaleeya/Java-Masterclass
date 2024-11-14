4e5d68ec55a9c880f29341a3e52370310d995c6e
Aaleeya <shaikhaaleeya17@gmail.com>
2024-11-14 14:07:56 -0500

    Added code for String manipulation methods

--- src/Strings/StringManipulation.java
+++ src/Strings/StringManipulation.java
@@ -31,5 +31,8 @@ public class StringManipulation {
         System.out.println("ABC\n".repeat(3).indent(8));
         System.out.println("-".repeat(20));
 
+        System.out.println("    ABC\n".repeat(3).indent(-2));
+        System.out.println("-".repeat(20));
+
     }
 }
---
+++ src/Strings/UsingStringBuilder.java
@@ -0,0 +1,25 @@
+package Strings;
+
+public class UsingStringBuilder {
+
+    public static void main(String[] args) {
+
+        String helloWorld = "Hello" + " World";
+        StringBuilder helloWorldBuilder = new StringBuilder("Hello" + " World");
+
+        printInformation(helloWorld);
+        printInformation(helloWorldBuilder);
+    }
+
+    public static void printInformation(String string){
+
+        System.out.println("String = "+string);
+        System.out.println("length = "+string.length());
+    }
+
+    public static void printInformation(StringBuilder builder){
+
+        System.out.println("String = "+builder);
+        System.out.println("length = "+builder.length());
+    }
+}
