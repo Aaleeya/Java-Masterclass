a13e7a12749a56c83e2c5633690485ea3cda0b0c
Aaleeya <shaikhaaleeya17@gmail.com>
2024-11-14 15:11:02 -0500

    Modified class UsingStringBuilder.java

--- src/Strings/UsingStringBuilder.java
+++ src/Strings/UsingStringBuilder.java
@@ -5,10 +5,22 @@ public class UsingStringBuilder {
     public static void main(String[] args) {
 
         String helloWorld = "Hello" + " World";
+        helloWorld.concat(" and Goodbye");
+
         StringBuilder helloWorldBuilder = new StringBuilder("Hello" + " World");
+        helloWorldBuilder.append(" and Goodbye");
 
         printInformation(helloWorld);
         printInformation(helloWorldBuilder);
+
+        StringBuilder emptyStart = new StringBuilder();
+        emptyStart.append("a".repeat(17));
+
+        StringBuilder emptyStart32 = new StringBuilder(32);
+        emptyStart32.append("a".repeat(17));
+
+        printInformation(emptyStart);
+        printInformation(emptyStart32);
     }
 
     public static void printInformation(String string){
@@ -21,5 +33,6 @@ public class UsingStringBuilder {
 
         System.out.println("String = "+builder);
         System.out.println("length = "+builder.length());
+        System.out.println("capacity = "+builder.capacity());
     }
 }
