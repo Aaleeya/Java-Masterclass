f34498b455680531d005dd1e0f6d7fcacff6e31a
Aaleeya <shaikhaaleeya17@gmail.com>
2024-11-17 15:50:59 -0500

    Array basics code added

---
+++ src/Arrays/Basics.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class Basics {
+
+    public static void main(String[] args) {
+
+        int[] myIntArray = new int[10];
+        myIntArray[5] = 50;
+
+        double[] myDoubleArray = new double[10];
+        myDoubleArray[2] = 7.5;
+
+        System.out.println(myDoubleArray[2]);
+
+        int[] firstFive = {1, 2, 3, 4, 5};
+        System.out.println("first = "+firstFive[0]);
+        int arrayLength = firstFive.length;
+        System.out.println("Length of array ="+arrayLength);
+
+        for (int i = 0; i < arrayLength; i++)
+            System.out.println("Element at position "+i + " = "+firstFive[i]);
+
+    }
+}
