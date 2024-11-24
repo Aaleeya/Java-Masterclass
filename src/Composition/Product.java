0b77e3970e636e164a3383e24cc6e1cac85bcd4f
Aaleeya <shaikhaaleeya17@gmail.com>
2024-11-24 18:44:45 -0500

    Composition code added

--- src/Composition/Product.java
+++ src/Composition/Product.java
@@ -16,14 +16,40 @@ public class Product {
 }
 
 class Monitor extends Product{
+
+    private int size;
+    private String resolution;
+
     public Monitor(String model, String manufacturer) {
         super(model, manufacturer);
+    }
+
+    public Monitor(String model, String manufacturer, int size, String resolution) {
+        super(model, manufacturer);
+        this.size = size;
+        this.resolution = resolution;
     }
 }
 
 class Motherboard extends Product{
+
+    private int ramSlots;
+    private int cardSlots;
+    private String bios;
+
     public Motherboard(String model, String manufacturer) {
         super(model, manufacturer);
+    }
+
+    public Motherboard(String model, String manufacturer, int ramSlots, int cardSlots, String bios) {
+        super(model, manufacturer);
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
+
+    public void loadProgram(String programName){
+        System.out.println("Program "+programName+ " is now loading...");
     }
 }
 
