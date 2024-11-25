6e0cfd7ca089e37660a2d133da2d926933e6a54d
Aaleeya <shaikhaaleeya17@gmail.com>
2024-11-25 16:05:29 -0500

    Encapsulation package added

---
+++ src/Encapsulation/Main.java
@@ -0,0 +1,15 @@
+package Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Player player = new Player();
+        player.name = "Alfie";
+        player.health = 20;
+        player.weapon = "bow and arrow";
+
+        int damage = 10;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = "+ player.healthRemaining());
+    }
+}
