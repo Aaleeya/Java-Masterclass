6ed0f8225f6fd8dfdfe9f5a5a0ce469d1b08e963
Aaleeya <shaikhaaleeya17@gmail.com>
2024-11-25 17:58:02 -0500

    Encapsulation package added

--- src/Encapsulation/Main.java
+++ src/Encapsulation/Main.java
@@ -3,13 +3,16 @@ package Encapsulation;
 public class Main {
     public static void main(String[] args) {
 
-        Player player = new Player();
-        player.name = "Alfie";
-        player.health = 20;
-        player.weapon = "bow and arrow";
+//        Player player = new Player();
+//        player.name = "Alfie";
+//        player.health = 20;
+//        player.weapon = "bow and arrow";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = "+ player.healthRemaining());
 
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("Remaining health = "+ player.healthRemaining());
+        EnhancedPlayer tim = new EnhancedPlayer("Tim", 200, "Sword");
+        System.out.println("Initial Health is "+ tim.healthRemaining());
     }
 }
