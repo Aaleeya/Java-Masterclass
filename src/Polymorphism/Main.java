dd91239e01f8006ec9e2ca272bd3355ce2e48a81
Aaleeya <shaikhaaleeya17@gmail.com>
2024-11-26 08:24:18 -0500

    Polymorphism package added

---
+++ src/Polymorphism/Main.java
@@ -0,0 +1,10 @@
+package Polymorphism;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Movie theMovie = new Adventure("Closer");
+        theMovie.watchMovie();
+    }
+}
