0f8cb19234ebb8b40fcfd99030489054f7cfd66b
Aaleeya <shaikhaaleeya17@gmail.com>
2024-11-28 17:01:22 -0500

    ArrayList code added

--- src/ArrayList/Main.java
+++ src/ArrayList/Main.java
@@ -1,5 +1,6 @@
 package ArrayList;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 record GroceryItem(String name, String type, int count){
@@ -18,5 +19,14 @@ public class Main {
         groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
         groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
         System.out.println(Arrays.toString(groceryArray));
+
+        //ArrayList objectList =  -> Raw use of parameterized class
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+        System.out.println(groceryList);
+
+        
+
      }
 }
