9a9e4a7999f581643104b1044b213f06ce19df31
Aaleeya <shaikhaaleeya17@gmail.com>
2024-11-29 13:41:19 -0500

    ArrayList code added

--- src/ArrayList/Main.java
+++ src/ArrayList/Main.java
@@ -8,6 +8,11 @@ record GroceryItem(String name, String type, int count){
     public GroceryItem(String name){
         this(name, "DAIRY", 1);
     }
+
+    @Override
+    public String toString(){
+        return String.format("%d %s in %s ", count, name.toUpperCase(), type); //this returns formated string %d-count
+    }                           // %s - 2 Strings- 1st one is name and 2nd is type
 }
 
 public class Main {
@@ -24,6 +29,16 @@ public class Main {
 
         ArrayList<GroceryItem> groceryList = new ArrayList<>();
         groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("milk"));
+        groceryList.add(new GroceryItem("oranges", "PRODUCE", 5));
+       // System.out.println(groceryList);
+
+        //to replace an item we can use set method
+        groceryList.set(0,
+                new GroceryItem("apples","PRODUCE", 6));
+
+        // to remove an item from the list
+        groceryList.remove(1);
         System.out.println(groceryList);
 
         
