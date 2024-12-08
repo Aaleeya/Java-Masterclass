a4855bdcb83a3fa55d660b62be745f863f77c3c4
Aaleeya <shaikhaaleeya17@gmail.com>
2024-12-08 15:17:43 -0500

    LinkedList challenge modified

--- src/Challenges/LinkedListChallenge.java
+++ src/Challenges/LinkedListChallenge.java
@@ -19,37 +19,66 @@ public class LinkedListChallenge {
         Place adelaide = new Place("Adelaide", 1374);
         addPlace(placesToVisit, adelaide);
         System.out.println(placesToVisit);
-//        String text = """
-//                Available actions(Select word or letter):
-//                (F)orward
-//                (B)ackward
-//                (L)ist Places
-//                (M)enu
-//                (Q)uit
-//                """;
-//        boolean flag = true;
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println(text);
-//        while(flag) {
-//            String letter = sc.nextLine().substring(0, 1);
-//
-//            switch(letter.toUpperCase()) {
-//                case "F" ->moveForward(placesToVisit);
-//                case "B" ->moveBackward(placesToVisit);
-//                case "L" -> System.out.println(placesToVisit);
-//                default -> flag=false;
-//            }
-//
-//        }
-//        sc.close();
+
+        var iterator = placesToVisit.listIterator();
+        Scanner sc = new Scanner(System.in);
+        boolean quitLoop = false;
+        boolean forward = true;
+
+        printMenu();
+
+        while(!quitLoop){
+
+            if(!iterator.hasPrevious()){
+                System.out.println("Originating : "+ iterator.next());
+                forward = true;
+            }
+
+            if(!iterator.hasNext()){
+                System.out.println("Final : "+ iterator.previous());
+                forward = false;
+            }
+
+            System.out.println("Enter value: ");
+            String menuItem = sc.nextLine().toLowerCase().substring(0, 1);
+
+           switch(menuItem) {
+                case "F" -> {System.out.println("User wants to go forward");
+                            break;}
+
+               case "B" -> {System.out.println("User wants to go backwards");
+                   break;}
+
+               case "M" -> {printMenu();
+                   break;}
+
+               case "L" -> {System.out.println(placesToVisit);
+                   break;}
+
+                default -> {quitLoop = true;
+                            break;}
+                }
+
+           }
+            sc.close();
     }
 
-    public static void addPlace(LinkedList<Place> list, Place place){
+    private static void addPlace(LinkedList<Place> list, Place place){
         if(list.contains(place)){
             System.out.println("Found duplicate: "+place);
             return;
         }
         list.add(place);
+    }
+
+    private static void printMenu(){
+        System.out.println("""
+                Available actions(Select word or letter):
+                (F)orward
+                (B)ackward
+                (L)ist Places
+                (M)enu
+                (Q)uit""");
     }
 
     public static void moveForward(LinkedList<String> list){
