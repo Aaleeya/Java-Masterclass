77bc99817eda1a8142f738e6becca098c4430d2d
Aaleeya <shaikhaaleeya17@gmail.com>
2024-12-07 22:50:29 -0500

    LinkedList challenge added

--- src/Challenges/LinkedListChallenge.java
+++ src/Challenges/LinkedListChallenge.java
@@ -3,38 +3,53 @@ package Challenges;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+record Place(String name, int distance){
+
+    @Override
+    public String toString() {
+        return String.format("%s (%d)",name,distance);
+    }
+}
+
 public class LinkedListChallenge {
 
-    private String town;
-    private int distance;
-
     public static void main(String[] args) {
-        LinkedList<String> placesToVisit = new LinkedList<>();
-        placesToVisit.add("Sydney");
-        placesToVisit.push("Melbourne");
-        String text = """
-                Available actions(Select word or letter):
-                (F)orward
-                (B)ackward
-                (L)ist Places
-                (M)enu
-                (Q)uit
-                """;
-        boolean flag = true;
-        Scanner sc = new Scanner(System.in);
-        System.out.println(text);
-        while(flag) {
-            String letter = sc.nextLine().substring(0, 1);
+        LinkedList<Place> placesToVisit = new LinkedList<>();
 
-            switch(letter.toUpperCase()) {
-                case "F" ->moveForward(placesToVisit);
-                case "B" ->moveBackward(placesToVisit);
-                case "L" -> System.out.println(placesToVisit);
-                default -> flag=false;
-            }
+        Place adelaide = new Place("Adelaide", 1374);
+        addPlace(placesToVisit, adelaide);
+        System.out.println(placesToVisit);
+//        String text = """
+//                Available actions(Select word or letter):
+//                (F)orward
+//                (B)ackward
+//                (L)ist Places
+//                (M)enu
+//                (Q)uit
+//                """;
+//        boolean flag = true;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(text);
+//        while(flag) {
+//            String letter = sc.nextLine().substring(0, 1);
+//
+//            switch(letter.toUpperCase()) {
+//                case "F" ->moveForward(placesToVisit);
+//                case "B" ->moveBackward(placesToVisit);
+//                case "L" -> System.out.println(placesToVisit);
+//                default -> flag=false;
+//            }
+//
+//        }
+//        sc.close();
+    }
 
+    public static void addPlace(LinkedList<Place> list, Place place){
+        if(list.contains(place)){
+            System.out.println("Found duplicate: "+place);
+            return;
         }
-        sc.close();
+        list.add(place);
     }
 
     public static void moveForward(LinkedList<String> list){
