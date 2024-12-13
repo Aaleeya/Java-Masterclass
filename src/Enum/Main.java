b386b9c303673a354af653aaa7e9dbc4c1431597
Aaleeya <shaikhaaleeya17@gmail.com>
2024-12-13 16:40:42 -0500

    Enum code added

--- src/Enum/Main.java
+++ src/Enum/Main.java
@@ -12,13 +12,28 @@ public class Main {
         for(int i = 0; i < 10; i++) {
             weekDay = getRandomeDay();
 
-            System.out.printf("Name is %s, Ordinal Value = %d%n", weekDay.name(),
-                    weekDay.ordinal());
-
-            if(weekDay == DayOfTheWeek.FRI){
-                System.out.println("Found Friday!!!");
-            }
+//            System.out.printf("Name is %s, Ordinal Value = %d%n", weekDay.name(),
+//                    weekDay.ordinal());
+//
+//            if(weekDay == DayOfTheWeek.FRI){
+//                System.out.println("Found Friday!!!");
+//            }
+            switchDayOfTheWeek(weekDay);
         }
+    }
+
+    public static void switchDayOfTheWeek(DayOfTheWeek weekDay){
+
+        int weekDayInteger = weekDay.ordinal() + 1;
+
+        switch (weekDay){
+            case WED -> System.out.println("Wednesday is Day "+weekDayInteger);
+            case SAT -> System.out.println("Saturday is Day "+weekDayInteger);
+            default -> System.out.println(weekDay.name().charAt(0)+
+                    weekDay.name().substring(1).toLowerCase()+
+                    "day is Day "+weekDayInteger);
+        }
+
     }
 
     public static DayOfTheWeek getRandomeDay(){
